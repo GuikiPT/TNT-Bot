49f8cb238e527e027d0001c11ae76ec28e269d5f
GuikiPT <foxzillapt17@gmail.com>
2023-06-07 22:25:48 +0100

    TODO later

--- commands/slashs/pokedex/pokemon.js
+++ commands/slashs/pokedex/pokemon.js
@@ -13,35 +13,40 @@ module.exports = {
                 .setRequired(true)
         ),
     async execute(interaction) {
-        try {
-            const pokemon = interaction.options.getString('pokemon');
-            const P = new Pokedex();
-            const pokeData = await P.getPokemonByName(pokemon.toLowerCase());
+        return interaction.reply({ content: 'This command is not available yet', ephemeral: true });
 
-            if (!pokeData) {
-                return await interaction.reply({ content: 'Pokémon not found', ephemeral: true });
-            }
+        // TODO: Manually create a wrap for pokeapi.co
+        // The pokedex-promise-v2 is not working properly
 
-            const pokeSpecieData = await P.getPokemonSpeciesByName(pokemon);
-            const pokeColor = await toHex(pokeSpecieData.color.name);
+    //     try {
+    //         const pokemon = interaction.options.getString('pokemon');
+    //         const P = new Pokedex();
+    //         const pokeData = P.getPokemonByName(pokemon.toLowerCase());
 
-            const englishFlavorTexts = pokeSpecieData.flavor_text_entries.filter(entry => entry.language.name === 'en');
-            const randomEnglishFlavorText = englishFlavorTexts[Math.floor(Math.random() * englishFlavorTexts.length)].flavor_text.replace(/(\r\n|\n|\r)/gm, " ");
+    //         // if pokemon undefined
+    //         if (!pokeData.name) return await interaction.reply({ content: 'Pokémon not found', ephemeral: true });
+            
 
-            const pokemonEmbed = new Discord.EmbedBuilder()
-                .setTitle(pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1))
-                // check why sprites of gen viii are not working
-                // .setThumbnail(pokeData.sprites.versions['generation-viii'].icons.front_default.toString())
-                .setColor(pokeColor ?? 'White')
-                .setDescription(randomEnglishFlavorText)
+    //         const pokeSpecieData = await P.getPokemonSpeciesByName(pokemon);
+    //         const pokeColor = await toHex(pokeSpecieData.color.name);
 
-                //TODO: add Fields Info/Data
-                .setTimestamp()
+    //         const englishFlavorTexts = pokeSpecieData.flavor_text_entries.filter(entry => entry.language.name === 'en');
+    //         const randomEnglishFlavorText = englishFlavorTexts[Math.floor(Math.random() * englishFlavorTexts.length)].flavor_text.replace(/(\r\n|\n|\r)/gm, " ");
 
-            return await interaction.reply({ embeds: [pokemonEmbed] });
-        }
-        catch (err) {
-            console.log(colors.red(err));
-        }
+    //         const pokemonEmbed = new Discord.EmbedBuilder()
+    //             .setTitle(pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1))
+    //             // check why sprites of gen viii are not working
+    //             // .setThumbnail(pokeData.sprites.versions['generation-viii'].icons.front_default.toString())
+    //             .setColor(pokeColor ?? 'White')
+    //             .setDescription(randomEnglishFlavorText)
+
+    //             //TODO: add Fields Info/Data
+    //             .setTimestamp()
+
+    //         return await interaction.reply({ embeds: [pokemonEmbed] });
+    //     }
+    //     catch (err) {
+    //         console.log(colors.red(err));
+    //     }
     },
 };
