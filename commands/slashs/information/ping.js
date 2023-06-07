9aa07b7ccc82aab36336b91f3a944f0bf353cb53
GuikiPT <foxzillapt17@gmail.com>
2023-06-07 16:21:03 +0100

    Ping Command Update

--- commands/slashs/information/ping.js
+++ commands/slashs/information/ping.js
@@ -1,10 +1,28 @@
 const Discord = require('discord.js');
+const colors = require('colors/safe');
 
 module.exports = {
 	data: new Discord.SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		return interaction.reply('Pong!');
+		try {
+			await interaction.reply({ content: '<a:DiscordLoading:1035119091662454836>', fetchReply: true }).then(async (reply) => {
+				const ping = reply.createdTimestamp - interaction.createdTimestamp;
+				const pingEmbed = new Discord.EmbedBuilder()
+					.setColor('Green')
+					.setTitle('🏓 | Pong!')
+					.setThumbnail(interaction.client.user.displayAvatarURL({ dynamic: false, size: 1024, format: 'png' }))
+					.addFields(
+						{ name: '**Bot Latency**', value: '```ini\n [ ' + ping + 'ms ]\n```', inline: false },
+						{ name: '**API Connection Latency**', value: '```ini\n [ ' + Math.round(interaction.client.ws.ping) + 'ms ]\n```', inline: false },
+						)
+					.setTimestamp()
+				await interaction.editReply({ content: '', embeds: [pingEmbed] });
+			});
+		}
+		catch (err) {
+			console.log(colors.red(err));
+		}
 	},
 };
